f025a7b24d9c9578af8714db4b732b6edc020234
Tharinda P Anurajeewa <tharindahp@gmail.com>
2021-06-08 22:40:15 +0530

    added material ui icons

--- src/screens/mainScreen/MainScreen.tsx
+++ src/screens/mainScreen/MainScreen.tsx
@@ -1,8 +1,26 @@
+import {
+  DefaultButton,
+  initializeIcons,
+  PrimaryButton,
+} from "@fluentui/react/lib";
+import { SearchBox } from "@fluentui/react/lib/SearchBox";
 import * as React from "react";
-import { Component } from "react";
 import TopBar from "../../components/topBar/TopBar";
 
 import "./mainScreen-style.css";
+
+import { setIconOptions } from "@fluentui/react/lib/Styling";
+
+import Menu from "@material-ui/icons/Menu";
+import User from "@material-ui/icons/Person";
+
+// Suppress icon warnings.
+setIconOptions({
+  disableWarnings: true,
+});
+// To avoid the issue of not showing icons in the
+// fluent ui controllers
+initializeIcons();
 
 const MainScreen = () => {
   return (
@@ -12,7 +30,13 @@ const MainScreen = () => {
       </div>
 
       <div className="actionBar">
-        <button>hello</button>
+        <Menu />
+        <PrimaryButton text="New Mail" />
+        <SearchBox
+          placeholder="Search"
+          onSearch={(newValue: string) => console.log("value is " + newValue)}
+        />
+        <User />
       </div>
     </div>
   );
